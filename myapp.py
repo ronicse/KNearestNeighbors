f6532de8b08b8685ab9bcf7ccb3364c0e64830c0
ronicse <roni_cse@yahoo.com>
2025-09-30 02:28:04 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -4,3 +4,18 @@ import numpy as np
 
 st.title("🔎 K-Nearest Neighbors - Build & Deploy (Streamlit)")
 st.sidebar.header("Dataset & Preprocessing")
+
+from sklearn.datasets import load_iris, load_wine, load_breast_cancer
+def load_sample(name):
+  if name == "Iris":
+    df = load_iris(as_frame=True)
+  elif name == "Wine":
+    df = load_wine(as_frame=True)
+  elif name == "Breast Cancer":
+    df = load_breast_cancer(as_frame=True)
+  else:
+    return None
+  df = pd.concat[df.frame.reset_index(drop=True)], axis=1]
+  return df
+
+data_source = st.sidebar.selectbox["Upload CSV", "Sample dataset (Iris)", "Sample dataset (Wine)", "Sample dataset (Breast Cancer)"]
