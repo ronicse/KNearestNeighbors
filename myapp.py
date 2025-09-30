8ce29fcfb741c0ff4c9435b6e007a3387576482e
ronicse <roni_cse@yahoo.com>
2025-09-30 02:59:31 +0200

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -7,11 +7,11 @@ st.sidebar.header("Dataset & Preprocessing")
 
 from sklearn.datasets import load_iris, load_wine, load_breast_cancer
 def load_sample(name):
-  if name == "Iris":
+  if name == "Sample dataset (Iris)":
     df = load_iris(as_frame=True)
-  elif name == "Wine":
+  elif name == "Sample dataset (Wine)":
     df = load_wine(as_frame=True)
-  elif name == "Breast Cancer":
+  elif name == "Sample dataset (Breast Cancer)":
     df = load_breast_cancer(as_frame=True)
   else:
     return None
@@ -19,3 +19,26 @@ def load_sample(name):
   return df
 
 data_source = st.sidebar.selectbox("Data Source", ["Upload CSV", "Sample dataset (Iris)", "Sample dataset (Wine)", "Sample dataset (Breast Cancer)"])
+
+if data_source == "Upload CSV":
+  uploaded = st.sidebar.file_uploader("Upload CSV", type=["csv", "txt"])
+  if uploaded is not None:
+    try:
+      df = pd.read_csv(uploaded)
+      df = df.dropna()
+      st.success("Loaded sample data.")
+    except Exception as e:
+      st.sidebar.error(f"Couldn't read file: {e}")
+      st.stop()
+  else:
+    st.info("Upload a CSV on the left or cloose a sample dataset to get started.")
+    st.stop()
+else:
+  df = load_sample(data_source)  
+
+st.write("### Dataset Preview")
+st.write(df.head())
+  
+
+
+
